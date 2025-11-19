e46681ba6e42effc834faa530ed5f08385080454
ibrarahmed88-hash <ibrarahmed88@eng.rizvi.edu.in>
2025-11-19 21:52:56 +0530

    Create Experiment no 2 (a).c

---
+++ Experiment no 2 (a).c
@@ -0,0 +1,26 @@
+/*
+Name: ibrar ahmed 
+UIN:251M034
+Class:FE Mechanical
+Div: F
+*/
+#include <stdio.h>
+
+int main() 
+{
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) 
+    {
+    printf("The number %d is even.\n", number);
+    } 
+    else 
+    {
+    printf("The number %d is odd.\n", number);
+    }
+
+    return 0;
+}
