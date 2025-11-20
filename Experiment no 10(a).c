dfa4bac0a8606f5de81f13e271051315087f0c95
ibrarahmed88-hash <ibrarahmed88@eng.rizvi.edu.in>
2025-11-20 20:29:09 +0530

    Create Experiment no 10(a).c

---
+++ Experiment no 10(a).c
@@ -0,0 +1,24 @@
+/*NAME:IBRAR AHMED 
+UIN:251M034
+BRANCH:FE MECHANICAL 
+DIV:F
+*/
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    int *p1, *p2;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    p1 = &a;
+    p2 = &b;
+    sum = *p1 + *p2;
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
